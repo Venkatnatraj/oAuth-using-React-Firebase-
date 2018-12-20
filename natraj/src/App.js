1606aae725a31f5206dab1542280eed34a0e0443
Venkatnatraj <t.venkatanatraj95@gmail.com>
2018-12-20 15:50:59 +0530

    Oauth implemented for google

--- natraj/src/App.js
+++ natraj/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   uiConfig = {
     signInFlow: "popup",
     signInOptions: [
-      // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
