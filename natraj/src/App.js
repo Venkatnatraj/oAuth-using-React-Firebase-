f2fbf8b9a1a9145a6acbaa15eacb6d123fbd0e37
Venkatnatraj <t.venkatanatraj95@gmail.com>
2018-12-20 15:52:24 +0530

    Oauth implemented

--- natraj/src/App.js
+++ natraj/src/App.js
@@ -15,9 +15,9 @@ class App extends Component {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      // firebase.auth.EmailAuthProvider.PROVIDER_ID
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     callbacks: {
       signInSuccess: () => false
