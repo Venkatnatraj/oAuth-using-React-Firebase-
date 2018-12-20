764f600e9500ee3fa29e8d961c0487b27ebd5d52
Venkatnatraj <t.venkatanatraj95@gmail.com>
2018-12-20 15:51:42 +0530

    Oauth implemented for github

--- natraj/src/App.js
+++ natraj/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     callbacks: {
